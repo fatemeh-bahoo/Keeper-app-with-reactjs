ad581461492baaa98582e991579f6f435a9cc391
fatemeh-bahoo <111791107+fatemeh-bahoo@users.noreply.github.com>
2023-08-13 15:55:06 +0330

    Update CreateArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,48 +1,62 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
 
-const [text , setText] = useState({
-  title:"",
-  content:""
-});
-
-function handleChange(event){
-  const {name , value} = event.target;
-  setText(prevText => {
-    return{
-      ...prevText,
-       [name]: value
-    };
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
   });
-}
 
-function clicked(event){
-  props.onAdd(text);
-  setText({
-    title:"",
-    content:""
-  });
-  event.preventDefault();
-}
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  function expand() {
+    setExpanded(true);
+  }
 
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          onChange={handleChange}
-          value={text.title}
-          placeholder="Title"
-        />
-        <textarea
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+
+        <textarea className="create-note"
           name="content"
+          onClick={expand}
           onChange={handleChange}
-          value={text.content}
+          value={note.content}
           placeholder="Take a note..."
-          rows="3" 
-/>
-        <button onClick={clicked}>Add</button>
+          rows={isExpanded ? 3 : 1}
+        />
+        {isExpanded &&
+
+          <button type="button" onClick={submitNote}>
+         <i class="bi bi-plus-lg"></i>         </button>
+        }
       </form>
     </div>
   );
